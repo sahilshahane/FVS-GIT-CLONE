92df9332a962f1a5c684fb4c2fac1fc75ef22f05
Uttkarsh09 <uttkarshpatel9@gmail.com>
2020-08-27 18:00:50 +0530

    version2

--- DrivePart/integrateAll.py
+++ DrivePart/integrateAll.py
@@ -9,9 +9,8 @@ fileIdDictionary = {}
 class UploadItBoi():
   def __init__(self):
     service = authenticate.get_gdrive_service()
-    fileCount = 0       # match the count of files and folders to be uploaded
-    folderCount = 0
   
+  # This is to upload the Files
   def generateIDmeta(self, folderPath, parentId="", FIRSTTIME=False):
     global folderIdDictionary
     folderName = os.path.basename(folderPath)
@@ -24,12 +23,12 @@ class UploadItBoi():
           parentId = folderIdDictionary[key]
         else:
           parentId = ""
-    
+  
     folderId = upload.createFolder(folderName, parentId)
     folderIdDictionary.__setitem__(folderPath, folderId)
     print(f"Uploaded -> {folderName} folderId -> {folderId} ParentId -> {folderId}")
-    folderCount += 1
 
+# To upload the files (NOT THE FIRST TIME)
   def putFiles(self, filePath, parentId=""):
     global folderIdDictionary
     global fileIdDictionary
@@ -46,7 +45,7 @@ class UploadItBoi():
     fileId = upload.uploadFile(filePath, parentId)
     fileIdDictionary.__setitem__(fileName, fileId)
     print(f"Uploaded -> {fileName} Fileid -> {fileId} ParentId -> {parentId}")
-    fileCount += 1
+    return fileId
 
   def readFolderId(self):
     with open("folderNameId.txt", "r") as f:
@@ -60,8 +59,9 @@ class UploadItBoi():
     global folderIdDictionary
 
     with open("fileNameId.txt", "w") as f:
+      # Here the "key" is not the id bitch dont't get confused (for uttkarsh)
       for key in fileIdDictionary:
-        f.write(f"{key}<!@#$%>{folderIdDictionary}\n")
+        f.write(f"{key}<!@#$%>{fileIdDictionary[key]}\n")
 
     with open("folderNameId.txt", "w") as f:
       for key in folderIdDictionary:
@@ -69,3 +69,27 @@ class UploadItBoi():
   
 uplo = UploadItBoi()
 
+# STEPS TO UPLOAD SH!T for the first time
+# 
+#  1. Call the generateIDmeta method to UPLOAD THE FOLDERS and create the DICTIONARY to with all the FOLDER NAMES and their ID.
+#   
+#  2. Create a FILE which has the FOLDER PATHS and their IDs by calling the method createMetaFiles().
+#
+#  3. Now you have the FOLDER IDs, you can upload the FILES to drive by calling the putFiles() method.
+#
+#  4. Create a FILE which has all the FILE PATHS and their IDs by calling the method createMetaFiles().
+#
+# 5. Now, you just have to integrate the newly created FILE and FOLDER IDs to the existing files
+#
+
+# STEPS TO UPLOAD SH!T not for the first time
+# 
+#  USE THE readFolderId() to get the dictionary of the FOLDER PATHS and their IDs.  
+#
+#  CREATING NEW FOLDERS
+#   1. Call the generateIDmeta() method without the FIRSTTIME arg as false or ignore it.
+#   2. The above step will also add to the dictionary
+#
+#  CREATING NEW FILES
+#   1. Call the putFiles() method 
+#   2. This will also add to the dictionary
